894bc535564fe65bfec9cbc4d023fdd3834766f0
Heyder Dias <heyderpd@gmail.com>
2018-08-07 22:53:02 -0300

    init EscapistWrapper

--- developer/src/App.js
+++ developer/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import EscapistWrapper from '../../src';
+import EscapistWrapper from './escapist-wrapper';
 
 class App extends Component {
   render() {
